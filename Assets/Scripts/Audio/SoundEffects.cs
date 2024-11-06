49eb53d434d5c908721956661cf2af37057dbf91
Alasdair Casperd <alasdaircasperd@gmail.com>
2024-11-06 23:44:38 +0000

    create example sound effects

--- Assets/Scripts/Audio/SoundEffects.cs
+++ Assets/Scripts/Audio/SoundEffects.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "SoundEffects", menuName = "Scriptable Objects/SoundEffects")]
 public class SoundEffects: ScriptableObject {
-
+    public SoundEffect explosion;
+    public SoundEffect thud;
+    public SoundEffect woodenSmash;
 }
