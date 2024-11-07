5535cd45cdc1745aa14925b32fe00974ab921201
Alasdair Casperd <alasdaircasperd@gmail.com>
2024-11-07 20:13:52 +0000

    add example background sound

--- Assets/Scripts/Audio/BackgroundSounds.cs
+++ Assets/Scripts/Audio/BackgroundSounds.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "BackgroundSounds", menuName = "Scriptable Objects/BackgroundSounds")]
 public class BackgroundSounds: ScriptableObject {
-
+    public BackgroundSound engineNoise;
 }
