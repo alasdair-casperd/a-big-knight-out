a11e6c29d0aeeab81574db8e71afdd75ad80a0ff
Alasdair Casperd <alasdaircasperd@gmail.com>
2024-12-15 14:12:22 +0000

    store on TileType whether the player can start on this type of tile

--- Assets/Scripts/Gameplay/TileType.cs
+++ Assets/Scripts/Gameplay/TileType.cs
@@ -12,12 +12,34 @@ public struct TileType
         TileType Properties
     */
 
+    /// <summary>
+    /// An ID for the tile type. This must be unique
+    /// </summary>
     public readonly int ID;
+
+    /// <summary>
+    /// A name to display in-game to represent this tile type
+    /// </summary>
     public readonly string DisplayName;
 
+    /// <summary>
+    /// The list of valid states this tile can have, e.g. {0, 1}
+    /// </summary>
     public readonly List<int> ValidStates;
+
+    /// <summary>
+    /// The list of the ID's of the tile types that this tile can be linked to
+    /// </summary>
     public readonly List<int> ValidLinkTargetIDs;
 
+    /// <summary>
+    /// Can a tile of this type be used as the start location for the player?
+    /// </summary>
+    public readonly bool IsValidStartPosition;
+
+    /// <summary>
+    /// The list of tile types that this tile can be linked to
+    /// </summary>
     public readonly List<TileType> ValidLinkTargets
     {
         get
@@ -27,34 +49,47 @@ public struct TileType
         }
     }
     
+    /// <summary>
+    /// Can this tile be given initial states?
+    /// </summary>
     public readonly bool IsMultiState
     {
         get { return ValidStates.Count > 0; }
     }
 
+    /// <summary>
+    /// Can this tile be linked to other tiles?
+    /// </summary>
     public readonly bool IsLinkable
     {
         get { return ValidLinkTargets.Count > 0; }
     }
 
     /*
-        TileType Properties
+        TileType Initialiser
     */
     
-    public TileType(int id, string displayName, List<int> validStates, List<int > validLinkTargetIDs)
+    public TileType(int id, string displayName, List<int> validStates, List<int > validLinkTargetIDs, bool isValidStartPosition = false)
     {
         ID = id;
         DisplayName = displayName;
         ValidStates = validStates;
         ValidLinkTargetIDs = validLinkTargetIDs;
+        IsValidStartPosition = isValidStartPosition;
     }  
+
+    /*
+        All of the game's TileTypes
+        (When adding a new one, make sure it add it to the list TileType.All)
+    */
 
     public static TileType Floor = new
     (
         id: 0,
         displayName: "Floor",
         validStates: new(),
-        validLinkTargetIDs: new()
+        validLinkTargetIDs: new(),
+        isValidStartPosition: true
     );
 
     public static TileType Wall = new
@@ -71,7 +106,8 @@ public struct TileType
         id: 2,
         displayName: "Falling Platform",
         validStates: new(),
-        validLinkTargetIDs: new()
+        validLinkTargetIDs: new(),
+        isValidStartPosition: true
     );
 
     public static TileType Portal = new
@@ -95,14 +131,20 @@ public struct TileType
         id: 5,
         displayName: "Spikes",
         validStates: new() { 0, 1 },
-        validLinkTargetIDs: new() { 5 }
+        validLinkTargetIDs: new() { 5 },
+        isValidStartPosition: true
     );
-    
-    /*
-        All of the game's TileTypes
-    */
 
-    public static TileType[] All = {Floor, Wall, FallingFloor, Portal, MovingPlatform, Spikes};
+    // A list of all the tile types
+    public static TileType[] All =
+    {
+        Floor,
+        Wall,
+        FallingFloor,
+        Portal,
+        MovingPlatform,
+        Spikes
+    };
 
     /*
         Operator Overloads
