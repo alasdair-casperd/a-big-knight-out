efda8daafade9883a0b399ea6b8cdc48675bf6c8
Scott Harrington <scottharringtonsd@gmail.com>
2025-02-18 11:30:51 +0000

    implement rooks

--- Assets/Scripts/Enemies/Rook.cs
+++ Assets/Scripts/Enemies/Rook.cs
@@ -15,4 +15,75 @@ public class Rook : Enemy
         }
     }
     public override int GraphicsVariant { get; set; }
+
+    public override void OnEnemyTurn()
+    {
+        CalculateNextSquare();
+    }
+
+    public void CalculateNextSquare()
+    {
+        int oppositeDirection;
+        bool shouldCapture = false;
+
+        if (Direction + 2 < 4)
+        {
+            oppositeDirection = Direction + 2;
+        }
+        else
+        {
+            oppositeDirection = Direction - 2;
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            shouldCapture = CheckNextSquareCapture(Position, i);
+            if (shouldCapture)
+            {
+                break;
+            }
+        }
+
+        if (shouldCapture)
+        {
+            NextSquare = PlayerController.position;
+            PlayerController.Die();
+        }
+        else if (SquareManager.squares.ContainsKey(Position + EnemyMove[Direction])
+            && SquareManager.squares[Position + EnemyMove[Direction]].IsPassable
+            && !CheckSquareForEnemy(Position + EnemyMove[Direction]))
+        {
+            NextSquare = Position + EnemyMove[Direction];
+        }
+        else if (SquareManager.squares.ContainsKey(Position + EnemyMove[oppositeDirection])
+            && SquareManager.squares[Position + EnemyMove[oppositeDirection]].IsPassable
+            && !CheckSquareForEnemy(Position + EnemyMove[oppositeDirection]))
+        {
+            Direction = oppositeDirection;
+            NextSquare = Position + EnemyMove[Direction];
+        }
+        else
+        {
+            NextSquare = Position;
+        }
+
+    }
+
+    public bool CheckNextSquareCapture(Vector2Int startSquare, int captureDirection)
+    {
+        if (SquareManager.squares.ContainsKey(startSquare + EnemyMove[captureDirection])
+            && SquareManager.squares[startSquare + EnemyMove[captureDirection]].IsPassable
+            && !CheckSquareForEnemy(startSquare + EnemyMove[captureDirection]))
+        {
+            if (startSquare + EnemyMove[captureDirection] == PlayerController.position)
+            {
+                return true;
+            }
+            else
+            {
+                return CheckNextSquareCapture(startSquare + EnemyMove[captureDirection], captureDirection);
+            }
+        }
+        return false;
+    }
 }
